2d4267eb9a457bbde6bd70bf3f91a6b7b703e296
Rickie <contato@rickie.com.br>
2025-09-30 20:39:49 -0300

    first commit

---
+++ src/IconsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RickieZera\Icons;
+
+use Illuminate\Support\ServiceProvider;
+use BladeUI\Icons\Factory;
+
+class IconsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot(Factory $factory)
+    {
+        // Registra os ícones diretamente do pacote
+        $factory->add('icons', __DIR__ . '/../resources/svg');
+
+        // Permite publicar os SVGs no projeto consumidor, se quiser sobrescrever
+        $this->publishes([
+            __DIR__ . '/../resources/svg' => resource_path('vendor/icons/svg'),
+        ], 'icons');
+    }
+}
